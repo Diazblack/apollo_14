8619911f71d151195273a4dacd274e5b8f56e690
Cesar Jolibois <diazcd85@gmail.com>
2018-10-12 11:05:47 -0600

    tests did not pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :astronauts, :except => [:edit, :update]
+  resources :astronauts, :except => [:edit, :update, :destroy]
 
 end
--- spec/features/users_visits_index_and_sees_astronauts_spec.rb
+++ spec/features/users_visits_index_and_sees_astronauts_spec.rb
@@ -2,17 +2,15 @@ require 'rails_helper'
 
 describe 'when user goes to index sees astronauts'  do
   it 'should sees astronauts' do
-  apollo = Mission.new(title: "Apollo 13", time_in_space: 32)
-  capricorn = Mission.new(title: "Capricorn 4", time_in_space: 75)
-  gemini = Mission.new(title: "Gemini 7", time_in_space: 50)
-  neil = Astronaut.new(name: "Neil Armstrong", age: 37, job: "Commander", missions: [apollo, capricorn, gemini])
+    apollo = Mission.new(title: "Apollo 13", time_in_space: 32)
+    capricorn = Mission.new(title: "Capricorn 4", time_in_space: 75)
+    gemini = Mission.new(title: "Gemini 7", time_in_space: 50)
+    neil = Astronaut.new(name: "Neil Armstrong", age: 37, job: "Commander", missions: [apollo, capricorn, gemini])
 
-  visit '/astronauts'
+    visit '/astronauts'
 
-  expect(page).to have_content(neil.name)
-  expect(page).to have_content(neil.age)
-  expect(page).to have_content(neil.job)
-  expect(page).to have_content(neil.name)
-
+      expect(page).to have_content("Name #{neil.name}")
+      expect(page).to have_content("Age #{neil.age}")
+      expect(page).to have_content("Job #{neil.job}")
   end
 end
